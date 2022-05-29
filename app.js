24441f64b94b0f4eca53599bf1d941227eee5f48
mauLovera <mauricioloveraedu@gmail.com>
2022-05-29 15:45:52 -0400

    add secret number gen method

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const game = {
   title: 'Guess the Number!',
 
-  /* Game Values */ 
-  biggestNum: 100, //prompt(`Assign highest value`), // Reassign this to user choice
-  smallestNum: 1, //prompt(`Assign Lowest Value`), // Reassign this to user choice
-  secretNum: null,
-  previousGuess: [], 
+  // Maybe have a easy normal hard mode //
+  /* Game Values */
+  biggestNum: 100,
+  smallestNum: 1, 
+  secretNum: null, // determined on start 
+  prevGuesses: [], // holds guesses in empty array 
 
-  /* Get Guess Method */ 
-  getGuess: function(guess) {
-    if (isNaN(guess)) {
-      return `Please input a valid number`
-    } else {
-    return prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.`)
-  }},
-  
-  /* Play method */
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+  play: function () {
+    // Initialize Secret Number Generator //
+    this.secretNumGen()
+  },
+  secretNumGen: function () {
+    this.secretNum = Math.floor(Math.random() * (
+      this.biggestNum - this.smallestNum + 1) + this.smallestNum)
+    return this.secretNum
+  },
 }
 
-// console.log(game)
-
+  console.log(game.secretNumGen())``
