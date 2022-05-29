c9aa1da9b75840a6bc848a82b71638425023ab3c
mauLovera <mauricioloveraedu@gmail.com>
2022-05-29 16:45:14 -0400

    add get guess functionality

--- app.js
+++ app.js
@@ -11,12 +11,26 @@ const game = {
   play: function () {
     // Initialize Secret Number Generator //
     this.secretNumGen()
+    // Ask player to start guessing // 
+    this.getGuess()
+    return this.getGuess()
   },
-  secretNumGen: function () {
+  secretNumGen: function () { // Works 
     this.secretNum = Math.floor(Math.random() * (
       this.biggestNum - this.smallestNum + 1) + this.smallestNum)
     return this.secretNum
   },
+  getGuess: function () {
+    let guesses = [] // holds what will be returned 
+    for (i = 0; i < 10; i++) {
+      guesses.push(1 /*prompt(`Put a Number`)*/) // 1 for testing because I hate prompt
+    }
+    this.prevGuesses = guesses.splice(0) 
+    return this.prevGuesses
+  }
 }
+game.play() 
 
-  console.log(game.secretNumGen())``
+console.log(game.secretNumGen())
+console.log(game.getGuess())
+console.log(game.play())
